06c67c482a9cde4699a00c0f42bb43a177d02169
Aline Gasparindo <aline.gasparindo@gmail.com>
2020-01-30 21:19:48 -0500

    finishing front page

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Flat.create!( [
+  {  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3 },
+
+  {  name: 'Nice place in Mile End',
+  address: '5333 Avenue Casgrain',
+  description: 'Nice place to spend the winter',
+  price_per_night: 100,
+  number_of_guests: 5 },
+
+  {  name: 'Cozy app at Old Port',
+  address: 'Rue de la Victoire, 55',
+  description: 'Close to everything, touristic attractions',
+  price_per_night: 200,
+  number_of_guests: 4 },
+
+])
